06361b9fb67e27721ba74aba238a91007e17ebdf
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-10-04 17:11:12 -0300

    Terminar - Dia 05 - Funcionalidades avançadas - tempo 24:12

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,56 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
 
+const socketio = require('socket.io')
+const http = require('http')
+
 const routes = require('./routes')
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server)
 const port = 3333
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-pzby4.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+})
+
+// Não realizar isso em produção
+// Deve-se utilizar banco de dados como REDIS
+const connectedUsers = {};
+
+// socket = dados da conexão do usuário
+io.on('connection', socket => {
+  // query da conexão recebida
+  console.log(socket.handshake.query)
+
+  // socket.id = id da conexão
+  console.log('Usuário conectado', socket.id)
+
+  const { user_id } = socket.handshake.query
+
+  // Array pois armazena no objecto como (connectedUsers{1234123412312412}) por exemplo
+  // a ideia é armazenar o id do usuario (user_id) com o id da conexão
+  connectedUsers[user_id] = socket.id;
+
+  // Emitir
+  // setTimeout(() => {
+  //   socket.emit('hello', 'World')
+  // }, 4000)
+
+  // Ouvir
+  // socket.on('omni', data => {
+  //   console.log(data)
+  // })
+})
+
+// Middleware para todas as rotas
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next()
 })
 
 app.use(cors())
@@ -19,4 +61,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
-import api from '../../services/api'
+import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
+import api from '../../services/api';
 
-import './styles.css'
+import './styles.css';
+import { request } from 'https';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([])
+  const [requests, setRequests] = useState([])
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user')
+    const socket = socketio('http://localhost:3333', {
+      query: {
+        user_id
+      }
+    })
+
+    socket.on('booking_request', data => {
+      console.log(data)
+    })
+
+    // Ouvir
+    // socket.on('hello', data => {
+    //   console.log(data)
+    // })
+
+    // Emitir
+    // socket.emit('omni', 'Stack')
+  }, [])
   
   useEffect(() => {
     async function loadSpots() {
@@ -23,6 +47,21 @@ export default function Dashboard() {
 
   return (
     <>
+    <ul className="notifications">
+      {requests.map(request => (
+        <li key={request._id}>
+          <p>
+            <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>
+              {request.spot.company}
+            </strong> para a data: 
+            <strong>
+              {request.date}
+            </strong>
+          </p>
+        </li>
+      ))}
+    </ul>
+
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
